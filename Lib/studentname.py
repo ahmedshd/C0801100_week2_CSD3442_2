8dafd2920ac5074f137c8413cfe3cb41f6f6c4d3
Ahmed <ahd22674@gmail.com>
2021-05-28 01:22:19 +0530

    studentname.py and time.py file added

---
+++ Lib/studentname.py
@@ -0,0 +1,8 @@
+def student():
+
+    print("First name: ")
+    firstName = input()
+    print("Last name: ")
+    lastName = input()
+    print("Names in reverse order with space: ")
+    print(lastName + " " + firstName)
---
+++ Lib/time.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+dateTime = datetime.now()
+dateTime = dateTime.strftime("%Y-%m-%d %H:%M:%S")
+print("Date and time: ")
+print(dateTime)
